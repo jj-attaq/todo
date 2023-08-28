7f1623717e2a5cbd3150b8bf196d082b77c02507
jj.attaq <jj.attaq@tutanota.com>
2023-08-28 17:27:46 +0200

    ShowList() prints json, added uuid to entries

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jj-attaq/todo
 go 1.19
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/google/uuid v1.3.1
--- src/commands/commands.go
+++ src/commands/commands.go
@@ -2,17 +2,23 @@ package commands
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"github.com/jj-attaq/todo/src/database"
-	"github.com/jj-attaq/todo/utils"
 	"os"
 	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/jj-attaq/todo/src/database"
+	"github.com/jj-attaq/todo/utils"
 )
 
+//var commands = []string{"add", "delete", "quit", "?", "show", "update"}
+
 type Entry struct {
-	id       int
-	item     string
-	finished bool
+	Id       int
+	Item     string
+	Finished bool
+	UniqueID string
 }
 
 func contains(s []string, str string) bool {
@@ -23,11 +29,6 @@ func contains(s []string, str string) bool {
 	}
 	return false
 }
-func Commands() []string {
-	var commands []string
-	commands = append(commands, "add", "delete", "quit", "?", "show", "update")
-	return commands
-}
 func Input(prompt string) string {
 	fmt.Printf("%s", prompt)
 	stdinScanner := bufio.NewScanner(os.Stdin)
@@ -35,8 +36,14 @@ func Input(prompt string) string {
 	output := stdinScanner.Text()
 	return output
 }
+func Commands() []string {
+	var commands []string
+	commands = append(commands, "add", "delete", "quit", "?", "show", "update")
+	return commands
+}
 func ExecCommand(input string) string {
 	var output string
+	//	if contains(commands, input) {
 	if contains(Commands(), input) {
 		output = input
 	} else {
@@ -49,7 +56,7 @@ func AddTable() {
 	utils.HandleError(err)
 
 	removeTable.RowsAffected()
-	table, err := database.ConnDB().Exec("CREATE TABLE list (id MEDIUMINT NOT NULL AUTO_INCREMENT, item VARCHAR(30), finished BOOL DEFAULT 0, PRIMARY KEY (id))")
+	table, err := database.ConnDB().Exec("CREATE TABLE list (id MEDIUMINT NOT NULL AUTO_INCREMENT, item VARCHAR(30), finished BOOL DEFAULT 0, PRIMARY KEY (id), uniqueID VARCHAR(255))")
 	utils.HandleError(err)
 
 	table.RowsAffected()
@@ -59,14 +66,15 @@ func AddTodo(todo string) {
 		fmt.Printf("Todo item is too long, must be below 30 characters.\n")
 		return
 	}
-	addTodo, err := database.ConnDB().Exec("INSERT INTO list (item) VALUES (?)", todo)
+	uniqueID := uuid.New()
+	addTodo, err := database.ConnDB().Exec("INSERT INTO list (item, uniqueID) VALUES (?, ?)", todo, uniqueID)
 	utils.HandleError(err)
 
 	addTodo.RowsAffected()
 }
 func RemoveTodo(todoId string) {
 	id, err := strconv.Atoi(todoId)
-	remove, err := database.ConnDB().Exec("DELETE FROM list WHERE ? = id", id)
+	remove, err := database.ConnDB().Exec("DELETE FROM list WHERE ? = Id", id)
 	utils.HandleError(err)
 
 	remove.RowsAffected()
@@ -79,7 +87,7 @@ func UpdateBool(status string, todoId string) {
 		isFinished = 0
 	}
 	id, err := strconv.Atoi(todoId)
-	prep, err := database.ConnDB().Prepare("UPDATE list SET finished = ? WHERE id = ?")
+	prep, err := database.ConnDB().Prepare("UPDATE list SET Finished = ? WHERE Id = ?")
 	utils.HandleError(err)
 
 	defer prep.Close()
@@ -92,9 +100,12 @@ func ShowList() {
 	var entry Entry
 	show, err := database.ConnDB().Query("SELECT * FROM list")
 	for show.Next() {
-		err = show.Scan(&entry.id, &entry.item, &entry.finished)
+		err = show.Scan(&entry.Id, &entry.Item, &entry.Finished, &entry.UniqueID)
 		utils.HandleError(err)
 
-		fmt.Printf("%+15v\n", entry)
+		res, err := json.MarshalIndent(entry, "", "    ")
+		utils.HandleError(err)
+
+		fmt.Printf("%+8v\n", string(res))
 	}
 }
